94512afd3c46cbb92015247aaea14ff3e29ce91e
Oscar Mier <voscarmv@gmail.com>
2020-05-03 19:50:22 -0500

    Found way to download file without need for link.

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,5 +1,7 @@
 class UploadController < ApplicationController
   def newfile
+    @filename ="#{Rails.root}/public/incoming/test.txt"
+    send_file(@filename, :filename => "test.txt")
   end
   def new
 
@@ -7,8 +9,6 @@ class UploadController < ApplicationController
   def create
     name = params[:upload][:file].original_filename
     path = File.join("public", "incoming", name)
-    puts "LALALALA"
-    puts name
     File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
     flash[:notice] = "File uploaded"
     redirect_to "/upload/new"
