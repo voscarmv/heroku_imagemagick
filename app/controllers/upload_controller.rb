89dff29a975144ab30199263b4cc09566f2729df
Oscar Mier <voscarmv@gmail.com>
2020-05-03 22:27:22 -0500

    Was able to upload and download file unchanged. Now work on editing with imagemagick heroku buildpack

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,16 +1,25 @@
 class UploadController < ApplicationController
+  # @name = nil
   def newfile
-    @filename ="#{Rails.root}/public/incoming/test.txt"
-    send_file(@filename, :filename => "test.txt")
+    # @filename ="#{Rails.root}/public/incoming/test.txt"
+    # send_file(@filename, :filename => "test.txt")
+
+    # text = "Hello, world!"
+    # text = session[:contenido]
+    # puts text
+    send_data(session[:contenido], filename: "out.txt")
   end
   def new
 
   end
   def create
-    name = params[:upload][:file].original_filename
-    path = File.join("public", "incoming", name)
-    File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
-    flash[:notice] = "File uploaded"
-    redirect_to "/upload/new"
+    @name = params[:upload][:file].read
+    # send_data(@name, filename: "out.txt")
+    # path = File.join("public", "incoming", "test.txt")
+    # File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
+    # flash[:notice] = "File uploaded"
+    session[:contenido] = nil
+    session[:contenido] = @name
+    redirect_to action: :newfile
   end
 end
